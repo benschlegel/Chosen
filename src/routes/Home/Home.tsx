57fafd333f68628b10787243713be1c7b6fa1f2c
Ben Schlegel <ben5.schlegel@gmail.com>
2022-12-30 09:41:03 +0100

    Add manual gestures

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -3,17 +3,25 @@ import { StatusBar } from "expo-status-bar";
 import type { StyleProp, ViewStyle } from "react-native";
 import { Platform, View, StyleSheet, Text, Dimensions } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Animated, { useSharedValue } from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 import type { Routes, StackNavigationProps } from "../../Routes";
 import IconButton from "../../components/Buttons/IconButton";
+import type { Pointer } from "../../types";
+import PointerElement from "../../components/Ball/PointerElement";
 
 const { width, height } = Dimensions.get("screen");
 
 const orangeGradient = ["#ff9068", "#fd746c"];
 
+//Limit of 12 from rn gesture handler
+const MAX_POINTERS = 12;
+
 export default function Home({
   navigation,
 }: StackNavigationProps<Routes, "Home">): React.ReactElement {
+  //Insets
   const insets = useSafeAreaInsets();
   const headerMarginStyle: StyleProp<ViewStyle> = {
     marginTop: insets.top + 32,
@@ -24,37 +32,100 @@ export default function Home({
       Platform.OS === "android" ? insets.bottom + 10 : insets.bottom + 4,
   };
 
+  //Gesture Handling
+  const trackedPointers: Animated.SharedValue<Pointer>[] = [];
+  const active = useSharedValue(false);
+
+  for (let i = 0; i < MAX_POINTERS; i++) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    trackedPointers[i] = useSharedValue<Pointer>({
+      visible: false,
+      x: 0,
+      y: 0,
+    });
+  }
+
+  const gesture = Gesture.Manual()
+    .onTouchesDown((e, manager) => {
+      for (const touch of e.changedTouches) {
+        trackedPointers[touch.id]!.value = {
+          visible: true,
+          x: touch.x,
+          y: touch.y,
+        };
+      }
+
+      if (e.numberOfTouches >= 2) {
+        console.log("touches down");
+        manager.activate();
+      }
+    })
+    .onTouchesMove((e, _manager) => {
+      for (const touch of e.changedTouches) {
+        trackedPointers[touch.id]!.value = {
+          visible: true,
+          x: touch.x,
+          y: touch.y,
+        };
+      }
+    })
+    .onTouchesUp((e, manager) => {
+      for (const touch of e.changedTouches) {
+        trackedPointers[touch.id]!.value = {
+          visible: false,
+          x: touch.x,
+          y: touch.y,
+        };
+      }
+
+      if (e.numberOfTouches === 0) {
+        manager.end();
+      }
+    })
+    .onStart(() => {
+      active.value = true;
+    })
+    .onEnd(() => {
+      active.value = false;
+    });
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <Canvas style={styles.canvasContainer}>
-        <Rect x={0} y={0} width={width} height={height}>
-          <LinearGradient
-            start={vec(0, 0)}
-            end={vec(width, height)}
-            colors={orangeGradient}
-          />
-        </Rect>
-      </Canvas>
-      <View style={styles.absoluteContainer}>
-        <View style={[styles.headerContainer, headerMarginStyle]}>
-          <Text style={styles.headerText}>Chosen</Text>
-        </View>
-        <View>
-          <Text style={styles.instructionText}>
-            Press anywhere to get started
-          </Text>
-        </View>
-        <View style={[styles.footerContainer, footerMarginStyle]}>
-          <IconButton text="Themes" iconName="md-brush-outline" />
-          <IconButton
-            text="Settings"
-            iconName="md-settings-outline"
-            onPress={() => navigation.navigate("Settings")}
-          />
-        </View>
+    <GestureDetector gesture={gesture}>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Canvas style={styles.canvasContainer}>
+          <Rect x={0} y={0} width={width} height={height}>
+            <LinearGradient
+              start={vec(0, 0)}
+              end={vec(width, height)}
+              colors={orangeGradient}
+            />
+          </Rect>
+        </Canvas>
+        <Animated.View style={styles.absoluteContainer}>
+          {/* Draw Pointers */}
+          {trackedPointers.map((pointer, index) => (
+            <PointerElement pointer={pointer} active={active} key={index} />
+          ))}
+          <View style={[styles.headerContainer, headerMarginStyle]}>
+            <Text style={styles.headerText}>Chosen</Text>
+          </View>
+          <View>
+            <Text style={styles.instructionText}>
+              Press anywhere to get started
+            </Text>
+          </View>
+          <View style={[styles.footerContainer, footerMarginStyle]}>
+            <IconButton text="Themes" iconName="md-brush-outline" />
+            <IconButton
+              text="Settings"
+              iconName="md-settings-outline"
+              onPress={() => navigation.navigate("Settings")}
+            />
+          </View>
+        </Animated.View>
       </View>
-    </View>
+    </GestureDetector>
   );
 }
 
@@ -83,7 +154,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "flex-end",
     flexDirection: "row",
-    // alignContent: "space-between",
   },
   instructionText: {
     opacity: 0.85,
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Pointer {
+  visible: boolean;
+  x: number;
+  y: number;
+}
